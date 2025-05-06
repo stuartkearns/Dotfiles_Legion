4b16c7a485c90ed14252c152dfef44fd5c8ce347
stuartkearns <stuartkearns@gmail.com>
2025-05-06 19:18:56 -0400

    Now I'm starting over

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,1 +1,31 @@
-Dotfiles_Legion/wezterm/.wezterm.lua
+-- Wezterm Config for Legion
+
+-- WEZTERM API - DO NOT CHANGE
+local wezterm = require 'wezterm'
+
+-- CONFIG BUILDER - DO NOT CHANGE
+local config = wezterm.config_builder()
+
+-- START CONFIGURATION SETTINGS
+
+-- Set color scheme:
+config.color_scheme = 'Argonaut'
+
+-- Set Font
+config.font = wezterm.font 'JetBrains Mono'
+
+
+
+-- Toggle Full Screen
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+-- END CONFIGURATION SETTINGS
+
+-- RETURN CONFIG TO WEZTERM - DO NOT CHANGE
+return config
